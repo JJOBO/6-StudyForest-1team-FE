c8f52662919fb587706bea03fcd2a19222b25122
root <root@DESKTOP-KS7E5CV.>
2025-03-27 18:44:22 +0900

    feat: 오늘의 집중 조회 부분

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import HomePage from "./pages/HomePage";
+import FocusPage from "./pages/FocusPage.jsx"  // FocusPage로 변경
+
+console.log(FocusPage)
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/study/:id/focus" element={<FocusPage/>} />  {/* 상세 페이지 라우팅 */}
       </Routes>
     </BrowserRouter>
   );
