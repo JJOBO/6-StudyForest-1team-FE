4b363b96f7320ef52024fe44820fe10ff1085d14
xdnjs7 <xdnjs77777@gmail.com>
2025-03-28 10:35:57 +0900

    fix:이미지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import HomePage from "./pages/HomePage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
