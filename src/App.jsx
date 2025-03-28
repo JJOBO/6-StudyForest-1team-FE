225a43f09cbefaf18597501d797dd4a51e71052f
xdnjs7 <xdnjs77777@gmail.com>
2025-03-28 10:35:57 +0900

    fix:이미지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/layout/Header";
-import HomePage from "./pages/HomePage";
-import HabitPage from "./pages/HabitPage";
+import Header from "./components/common/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="study">
+        <Route path="/" element={<Home />} />
+        <Route path=":studyId">
+          <Route index element={<StudyPage />} />
           <Route path="habits" element={<HabitPage />} />
+          <Route path="focus" element={<FocusPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
