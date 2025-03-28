c37c4488ca565f0051dbdcd2255b8bdbf3b83809
fiivxyxxng <scx200800@gmail.com>
2025-03-28 14:33:43 +0900

    feat: Home 컴포넌트 경로 수정, 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/layout/Header";
+import Header from "./components/common/Header";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +9,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path=":studyId">
+          <Route index element={<StudyPage />} />
+          <Route path="habits" element={<HabitPage />} />
+          <Route path="focus" element={<FocusPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
