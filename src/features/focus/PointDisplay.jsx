2aaeb295e5a87f2ad97189448b293546de08f140
elisaohh <elisaohh@gmail.com>
2025-03-31 11:33:43 +0900

    feat: 집중 타이머 포인트 반영

--- src/features/focus/PointDisplay.jsx
+++ src/features/focus/PointDisplay.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import styles from './PointDisplay.module.scss';
+import React from "react";
+import styles from "./PointDisplay.module.scss";
 
-const PointDisplay = ({ points }) => {
+const PointDisplay = ({ totalPoints }) => {
   return (
     <div className={styles.points}>
-      <span>현재까지 획득한 포인트</span>
-      <div className={styles.pointsValue}>{points}P 획득</div>
+      <span>현재까지 획득한 포인트: {totalPoints}P</span>
     </div>
   );
 };
-
 
 export default PointDisplay;
