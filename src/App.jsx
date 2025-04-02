cc5ced42db045d5959e5eccd2ca03b885569d488
fiivxyxxng <scx200800@gmail.com>
2025-04-02 09:18:18 +0900

    feat: 습관 리렌더링 context 구현, modal 입력값 검증 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import FocusPage from "./pages/FocusPage";
 import HabitPage from "./pages/HabitPage";
 import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
+import { HabitProvider } from "./components/features/study/habit/HabitContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/registration" element={<StudyRegistration />} />
-          <Route path=":studyId" element={<StudyDetail />} />
-          <Route path=":studyId/focus" element={<FocusPage />} />
-          <Route path=":studyId/habits" element={<HabitPage />} />
-        </Route>
-      </Routes>
+      <HabitProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/registration" element={<StudyRegistration />} />
+            <Route path=":studyId" element={<StudyDetail />} />
+            <Route path=":studyId/focus" element={<FocusPage />} />
+            <Route path=":studyId/habits" element={<HabitPage />} />
+          </Route>
+        </Routes>
+      </HabitProvider>
     </BrowserRouter>
   );
 }
