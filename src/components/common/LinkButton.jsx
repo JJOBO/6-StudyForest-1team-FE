9a9d7f8e91b3d04ab5b52ba6c1900530c61d4fbc
xdnjs7 <xdnjs77777@gmail.com>
2025-04-02 16:00:16 +0900

    fix:포인트 배경 오류

--- src/components/common/LinkButton.jsx
+++ src/components/common/LinkButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Added import for navigation
+import { useNavigate } from "react-router-dom";
 import styles from "./LinkButton.module.scss";
 import arrowIcon from "../../assets/icons/ic_arrow_right.svg";
 
 function LinkButton({ type, onClick, studyId }) {
-  const navigate = useNavigate(); // Initialize navigation
+  const navigate = useNavigate();
 
   const linkPaths = {
     habit: `/${studyId}/habits`,
@@ -13,8 +13,8 @@ function LinkButton({ type, onClick, studyId }) {
   };
 
   const handleClick = () => {
-    if (onClick) onClick(); // Call the provided onClick handler if it exists
-    if (linkPaths[type]) navigate(linkPaths[type]); // Navigate to the corresponding path
+    if (onClick) onClick();
+    if (linkPaths[type]) navigate(linkPaths[type]);
   };
 
   const renderContent = () => {
