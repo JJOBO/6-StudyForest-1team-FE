a8a2d531a82c0a761cf00388450abae27392858b
jyeon03 <bjlyeon2003@naver.com>
2025-04-03 13:29:01 +0900

    홈버튼 링크 수정

--- src/components/features/focus/FocusContainer.jsx
+++ src/components/features/focus/FocusContainer.jsx
@@ -5,19 +5,20 @@ import styles from "./FocusContainer.module.scss";
 
 const FocusContainer = ({ studyInfo }) => {
   const { studyId } = useParams(); // studyId를 URL에서 가져오기
-  
 
   return (
     <div className={styles.header}>
       <div className={styles.titleContainer}>
-        <h1>{studyInfo.creatorNick}의 {studyInfo.name}</h1>
+        <h1>
+          {studyInfo.creatorNick}의 {studyInfo.name}
+        </h1>
       </div>
       <div className={styles.btnContainer}>
         <Link to={`/${studyId}/habits`} className={styles.btn}>
           오늘의 습관
           <img src={arrow} alt="arrow" />
         </Link>
-        <Link to="/" className={styles.btn}>
+        <Link to={`/${studyId}`} className={styles.btn}>
           홈<img src={arrow} alt="arrow" />
         </Link>
       </div>
