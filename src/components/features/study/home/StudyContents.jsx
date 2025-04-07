f8776bdb86176a423a8f6bffcddf7e01d9d35066
fiivxyxxng <scx200800@gmail.com>
2025-04-07 13:42:33 +0900

    fix: 드롭다운 리렌더링 에러 해결

--- src/components/features/study/home/StudyContents.jsx
+++ src/components/features/study/home/StudyContents.jsx
@@ -1,88 +1,107 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  memo,
+  useMemo,
+} from "react";
 import studyAPI from "../studyAPI";
 import styles from "./StudyContents.module.scss";
 import StudyCard from "./StudyCard";
 import { Link } from "react-router-dom";
 import searchIcon from "../../../../../src/assets/icons/ic_search.svg";
 
-function StudyContents() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortOption, setSortOption] = useState("createdAt");
+// 정렬 드롭다운 컴포넌트를 분리하여 메모이제이션
+const SortDropdown = memo(({ sortOption, onSortChange }) => {
   const [isSortOpen, setIsSortOpen] = useState(false);
-  const [cards, setCards] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [isInitialLoading, setIsInitialLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState(""); // 실제 검색에 사용될 쿼리
   const sortRef = useRef(null);
 
+  // 드롭다운 외부 클릭 핸들러
   useEffect(() => {
-    const fetchStudies = async () => {
-      if (offset === 0) setIsInitialLoading(true);
-      setError(null);
-      try {
-        const data = await studyAPI.getStudyList(
-          searchQuery, // searchQuery 사용
-          sortOption,
-          offset
-        );
-        setCards((prevCards) =>
-          offset === 0 ? [...data.studies] : [...prevCards, ...data.studies]
-        );
-        setTotal(data.total);
-      } catch (err) {
-        setError(err);
-      } finally {
-        if (offset === 0) setIsInitialLoading(false);
+    const handleClickOutside = (e) => {
+      if (e.target && sortRef.current && !sortRef.current.contains(e.target)) {
+        setIsSortOpen(false);
       }
     };
-
-    // searchQuery가 변경될 때만 fetchStudies 실행
-    if (searchQuery !== "" || offset > 0) {
-      fetchStudies();
-    } else if (offset === 0) {
-      fetchStudies();
-    }
 
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [searchQuery, sortOption, offset]);
+  }, []);
 
-  const handleClickOutside = (e) => {
-    if (sortRef.current && !sortRef.current.contains(e.target)) {
-      setIsSortOpen(false);
-    }
-  };
-
-  const handleLoadMore = () => {
-    if (cards.length < total) {
-      setOffset((prevOffset) => prevOffset + 6);
-    }
-  };
-
-  const handleSortChange = (option) => {
-    setCards([]);
-    setOffset(0);
+  const handleSortSelect = useCallback((option) => {
+    let newSortOption;
     switch (option) {
       case "최근 순":
-        setSortOption("createdAt");
+        newSortOption = "createdAt";
         break;
       case "오래된 순":
-        setSortOption("oldest");
+        newSortOption = "oldest";
         break;
       case "많은 포인트 순":
-        setSortOption("totalPointsDesc");
+        newSortOption = "totalPointsDesc";
         break;
       case "적은 포인트 순":
-        setSortOption("totalPointsAsc");
+        newSortOption = "totalPointsAsc";
         break;
       default:
-        setSortOption("createdAt");
+        newSortOption = "createdAt";
     }
-  };
+
+    if (newSortOption != sortOption) {
+      onSortChange(newSortOption);
+    }
+  }, []);
+
+  const label = useMemo(() => {
+    return {
+      createdAt: "최근 순",
+      oldest: "오래된 순",
+      totalPointsDesc: "많은 포인트 순",
+      totalPointsAsc: "적은 포인트 순",
+    }[sortOption];
+  }, [sortOption]);
+
+  const options = ["최근 순", "오래된 순", "많은 포인트 순", "적은 포인트 순"];
+
+  return (
+    <div className={styles.sortDropdown} ref={sortRef}>
+      <button
+        className={styles.sortButton}
+        onClick={() => setIsSortOpen((prev) => !prev)}
+      >
+        <span className={styles.sortLabel}>{label}</span>
+        <span className={styles.sortIcon}></span>
+      </button>
+      {isSortOpen && (
+        <ul className={styles.sortList}>
+          {options.map((option) => (
+            <li
+              key={option}
+              onClick={() => handleSortSelect(option)}
+              className={styles.sortItem}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+});
+
+function StudyContents() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOption, setSortOption] = useState("createdAt");
+  const [cards, setCards] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const isLoadingMoreRef = useRef(false);
 
   const calculateDays = (createdAt) => {
     const createdDate = new Date(createdAt);
@@ -99,10 +118,48 @@ function StudyContents() {
       setSearchQuery(searchTerm); // 실제 검색 쿼리 업데이트
     }
   };
-  // 최초 로딩일 때만 전체 로딩 화면 표시
+
+  // 정렬 변경 함수
+  const handleSortChange = useCallback((newSortOption) => {
+    setCards([]);
+    setOffset(0);
+    setSortOption(newSortOption);
+  }, []);
+
+  // 더보기 실행 함수
+  const handleLoadMore = useCallback(() => {
+    if (cards.length < total && !isLoadingMoreRef.current) {
+      isLoadingMoreRef.current = true;
+      setOffset((prevOffset) => prevOffset + 6);
+    }
+  }, [cards.length, total]);
+
+  // 데이터 패칭 함수
+  const fetchStudies = useCallback(async () => {
+    if (offset === 0) setIsInitialLoading(true);
+    setError(null);
+
+    try {
+      const data = await studyAPI.getStudyList(searchQuery, sortOption, offset);
+
+      setCards((prevCards) =>
+        offset === 0 ? [...data.studies] : [...prevCards, ...data.studies]
+      );
+      setTotal(data.total);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsInitialLoading(false);
+      isLoadingMoreRef.current = false;
+    }
+  }, [searchQuery, sortOption, offset]);
+
+  // API 호출 useEffect
+  useEffect(() => {
+    fetchStudies();
+  }, [fetchStudies]);
 
   if (isInitialLoading) return;
-
   if (error) {
     return <div className={styles.error}>Error: {error.message}</div>;
   }
@@ -120,44 +177,12 @@ function StudyContents() {
             placeholder="검색"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={handleKeyPress} // 엔터 키 이벤트 핸들러 추가
+            onKeyDown={handleKeyPress}
             className={styles.searchInput}
           />
         </div>
 
-        <div className={styles.sortDropdown} ref={sortRef}>
-          <button
-            className={styles.sortButton}
-            onClick={() => setIsSortOpen((prev) => !prev)}
-          >
-            <span className={styles.sortLabel}>
-              {
-                {
-                  createdAt: "최근 순",
-                  oldest: "오래된 순",
-                  totalPointsDesc: "많은 포인트 순",
-                  totalPointsAsc: "적은 포인트 순",
-                }[sortOption]
-              }
-            </span>
-            <span className={styles.sortIcon}></span>
-          </button>
-          {isSortOpen && (
-            <ul className={styles.sortList}>
-              {["최근 순", "오래된 순", "많은 포인트 순", "적은 포인트 순"].map(
-                (option) => (
-                  <li
-                    key={option}
-                    onClick={() => handleSortChange(option)}
-                    className={styles.sortItem}
-                  >
-                    {option}
-                  </li>
-                )
-              )}
-            </ul>
-          )}
-        </div>
+        <SortDropdown sortOption={sortOption} onSortChange={handleSortChange} />
       </div>
 
       <div className={styles.studyCards}>
@@ -183,6 +208,7 @@ function StudyContents() {
           </Link>
         ))}
       </div>
+
       {cards.length < total && (
         <div className={styles.loadMoreWrapper}>
           <button className={styles.loadMore} onClick={handleLoadMore}>
